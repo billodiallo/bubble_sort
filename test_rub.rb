4650aebdd9773d64463c875134a4141a2afa4b2d
Billo Diallo <billodiallonet@.com>
2021-02-10 17:11:30 +0300

    fix linters errors

--- test_rub.rb
+++ test_rub.rb
@@ -13,23 +13,4 @@ def bubble_sort(arr)
   arr
 end
 
- puts bubble_sort([6, 3, 88, 4, 32, 5, 10, 1, 55, 34])
-
-# BUBBLE SORT BY START
-def bubble_sort_by(array)
-  loop do
-    attempt2= false
-
-    (array.size - 1).times do |index|
-      if yield(array[index], array[index + 1])
-        array[index], array[index + 1] = array[index + 1], array[index]
-        attempt2= true
-      end
-    end
-
-    break unless flag
-  end
-  array
-end
-
-
+puts bubble_sort([6, 3, 88, 4, 32, 5, 10, 1, 55, 34])
